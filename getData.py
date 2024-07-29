bdf4e364b1dc2862d013967e2357571a025cba9d
yunchien77 <b812110004@tmu.edu.tw>
2024-07-29 10:10:50 +0800

    linkedin with ragic in background

--- getData.py
+++ getData.py
@@ -2,45 +2,32 @@ import requests
 from dotenv import load_dotenv
 import os
 
+load_dotenv()
+API_KEY = 'b0hPOFAzbkRaNzFJV1F4RzNkRTA1Sko3QS9BMVlrbzdUZWtzVi9FcGtzejVFTzlDMGxHMnMwVzVQaFpEcGdzT29JOENFVy9qNzZnPQ=='
+
+SERVER_URL = 'ap12.ragic.com'
+ACCOUNT_NAME = 'cancerfree'
+TAB = 'forms5'
+SHEET_INDEX = '4'
+
+FIELD_ID = '1002023'
+FIELD_LINK = '1002999'
+
+params = {
+    'api': '',
+    'v': 3,
+    'subtables': 0
+}
+
+API_ENDPOINT_LISTING_PAGE = f'https://{SERVER_URL}/{ACCOUNT_NAME}/{TAB}/{SHEET_INDEX}'
+
+response = requests.get(API_ENDPOINT_LISTING_PAGE, params=params, headers={'Authorization': 'Basic '+API_KEY})
+
 def getAllPeople():
-    load_dotenv()
-    API_KEY = 'b0hPOFAzbkRaNzFJV1F4RzNkRTA1Sko3QS9BMVlrbzdUZWtzVi9FcGtzejVFTzlDMGxHMnMwVzVQaFpEcGdzT29JOENFVy9qNzZnPQ=='
-
-
-    SERVER_URL = 'ap12.ragic.com'
-    ACCOUNT_NAME = 'cancerfree'
-    TAB = 'forms5'
-    SHEET_INDEX = '4'
-
-    params = {
-        'api': '',
-        'v': 3,
-        'subtables': 0
-    }
-
-    API_ENDPOINT_LISTING_PAGE = f'https://{SERVER_URL}/{ACCOUNT_NAME}/{TAB}/{SHEET_INDEX}'
-
     response = requests.get(API_ENDPOINT_LISTING_PAGE, params=params, headers={'Authorization': 'Basic '+API_KEY})
     #print(response.text)
 
     data = response.json()
-    '''
-    if data:
-        people_list = []
-        for key, value in data.items():
-            if value['相關連結'] == '':
-                people_list.append({
-                    'id': value['ID'],
-                    'name': value['Name'],
-                    'company': value['Company']
-                })
-                count = 0
-    for i in people_list:
-        print(i)
-        count+=1
-    print(count)
-
-    '''
 
     if data:
         people_list = []
@@ -48,11 +35,19 @@ def getAllPeople():
             if value['相關連結'] == '':
                 search_str = value['Name'] + ' ' + value['Company']
                 people_list.append({
-                    "id": value['ID'],
+                    "id": key,
                     "search": search_str
                 })
-    '''
-    for i in people_list:
-        print(i['id'], i['search'])
-    '''
     return people_list
+
+def updateData(RECORD_ID, result):
+    print(RECORD_ID, result)
+    API_ENDPOINT_FORM_PAGE = f'https://{SERVER_URL}/{ACCOUNT_NAME}/{TAB}/{SHEET_INDEX}/{RECORD_ID}'
+    
+    data = {
+        FIELD_LINK: result
+    }
+    requests.post(API_ENDPOINT_FORM_PAGE, params=params, json=data, headers={'Authorization': 'Basic '+API_KEY})
+
+
+getAllPeople()
--- linkedin.py
+++ linkedin.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+from getData import getAllPeople, updateData
 import logging
 import pandas as pd
 import os
@@ -36,50 +37,99 @@ def sign_in(driver):
     
     driver.implicitly_wait(15)
 
-def perform_person_search(driver):
+def perform_person_search(driver, search_str):
     # 搜尋框
-    search_box = driver.find_element(By.CSS_SELECTOR, '#global-nav-typeahead > input')
-    person_name = input("\nPlease specify the person's name that you are searching for: \n")
-
-    search_box.send_keys(person_name)
+    #search_box = driver.find_element(By.CSS_SELECTOR, '#global-nav-typeahead > input')
+    search_box = driver.find_element(By.CLASS_NAME, "search-global-typeahead__input")
+    #<input class="search-global-typeahead__input" placeholder="Search" role="combobox" aria-autocomplete="list" aria-label="Search" aria-activedescendant="" aria-expanded="false" type="text">
+    #search_box = driver.find_element(By.XPATH, "/html/body/div[6]/header/div/div/div/div[1]/input")
+    #search_str = input("\nPlease specify the person's name that you are searching for: \n")
+    search_box.send_keys(search_str)
     search_box.send_keys(Keys.ENTER)
     
     # 會員分類
     people_button = driver.find_element(By.CSS_SELECTOR, '#search-reusables__filters-bar > ul > li:nth-child(1) > button')
+    #people_button = driver.find_element(By.XPATH, '/div/section/div/nav/div/ul/li[1]/div/button')
     people_button.click()
-    return person_name
+
+    search_box.clear()
+
+def perform_person(driver, search_str):
+    # 搜尋框
+    search_box = driver.find_element(By.CLASS_NAME, "search-global-typeahead__input")
+    search_box.send_keys(search_str)
+    search_box.send_keys(Keys.ENTER)
+    time.sleep(3)
+    search_box.clear()
 
 def scrape_people(driver):
-    try:
-        #no_matching_people = driver.find_element(By.XPATH, "/html/body/div[6]/div[3]/div[2]/div/div[1]/main/div/div/div/section/h2")
-        #print(no_matching_people.text)
-        #print('\nNo matching people found.')
+    print('\n---> Person search begins....\n')
 
+    # 檢查是否有 "No results found" 消息
+    try:
         no_matching = driver.find_element(By.XPATH, "//h2[text()='No results found']")
         print(no_matching.text)
-        
+        return 'None', 'None'
     except NoSuchElementException:
-        print('\n---> Person search begins....\n')
-        
+        pass
+
+    # 嘗試獲取搜索結果
+    try:
         result = driver.find_element(By.CSS_SELECTOR, "li.reusable-search__result-container")
         
-        name_element = result.find_element(By.CSS_SELECTOR, "span.entity-result__title-text a span[aria-hidden='true']")
-        link_element = result.find_element(By.CSS_SELECTOR, "span.entity-result__title-text a")
-        
-        name = name_element.text
-        link = link_element.get_attribute('href')
-        
-        print(f"Name: {name}")
-        print(f"Link: {link}")
+        try:
+            #name_element = result.find_element(By.CSS_SELECTOR, "span.entity-result__title-text")
+            name_element = result.find_element(By.CSS_SELECTOR, "span.entity-result__title-text a span[aria-hidden='true']")
+            name = name_element.text.strip()
+        except NoSuchElementException:
+            name = "Name not available"
+
+        # 嘗試獲取鏈接
+        try:
+            link_element = result.find_element(By.CSS_SELECTOR, "span.entity-result__title-text a")
+            link = link_element.get_attribute('href')
+        except NoSuchElementException:
+            link = "Link not available"
+
+        if name != 'Name not available' and link != 'Name not available':
+            print(f"Name: {name}")
+            print(f"Link: {link}")
+            return name, link
+        else:
+            return 'None', 'None'
+
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        return 'Error', 'Error'
     
 def close_browser(driver):
     print(f'\nEnd Of Person Search!')
     driver.close()
 
-if __name__ == "__main__":
+def search():
     driver = init_driver()
     navigate_to_linkedin(driver)
     sign_in(driver)
-    person_name = perform_person_search(driver)
-    information = scrape_people(driver)
+
+    people_list = getAllPeople()
+    count = 0
+
+    for person in people_list:
+        name = person['search']
+        print(f'-----------{name}--------------')
+        if count == 0:
+            perform_person_search(driver, name)
+        else: 
+            perform_person(driver, person['search'])
+        name, link = scrape_people(driver)
+        
+        if name != 'None' and link != 'None':
+            updateData(person['id'], name + ' ' + link) #updata data to Ragic
+
+        count += 1
+        #time.sleep(1)
+    
     close_browser(driver)
+
+if __name__ == "__main__":
+    search()
